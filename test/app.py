b7dbf96e73f28e831501cbe4538efb4eb7816e5b
Manuel Pereira <manuel.pereira@edu.esiee.fr>
2021-11-08 17:09:03 +0100

    mise en place de la recherche

--- test/app.py
+++ test/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import pymongo
 from pymongo import MongoClient
+
 
 #Initialisation de Flask
 app = Flask(__name__)
@@ -23,10 +24,10 @@ def get_db():
 #On met en place la route du Flask
 @app.route('/')
 def ping_server():
-    return "Main page du projet FullStack"
+    return "<h1>Main page du projet FullStack</h1>"
 
 #On met une nouvelle route pour pouvoir interroger la database et aller chercher les données
-@app.route('/wiki')
+@app.route('/wiki/all')
 #fonction afin de pouvoir récupérer les informations
 #_wiki = db.wiki_tb.find() => Il permet d'aller chercher dans la table wiki_tb
 def fetch_wiki(): 
@@ -34,6 +35,24 @@ def fetch_wiki():
     _wiki = db.wiki_tb.find() 
     wikis = [{'id' : wiki['id'], 'name' : wiki['name']} for wiki in _wiki]
     return jsonify({'wikis': wikis})
+
+#On fait ça afin de pouvoir rechercher les articles dans l'URL 
+#Ainsi on 
+@app.route('/wiki')
+def search_wiki():
+    db = get_db()
+    _wiki = db.wiki_tb.find() 
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return("Erreur : Indiquez une ID svp")
+    results=[]
+    for wiki in _wiki:
+        if wiki['id'] == id:
+            results.append({'id' : wiki['id'], 'name' : wiki['name']})
+    return jsonify(results)
+
+    
 
 #Sert à lancer le serveur au lancement du programme
 if __name__ == '__main__':
